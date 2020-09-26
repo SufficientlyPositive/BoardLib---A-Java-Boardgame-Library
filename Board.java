404d99622864f025d561aaa073e85ce8f755ebeb
ASmallFish-MD <62806641+ASmallFish-MD@users.noreply.github.com>
2020-09-26 14:39:27 +0100

    Add files via upload

---
+++ Board.java
@@ -0,0 +1,16 @@
+/**
+ * Class, for holding tile instances, should include the following functions:
+ * 
+ * move(*some coordinates*, *some coordinates*) return boolean
+ * isPiece(*some coordinates*) return boolean
+ * euclideanDistance(*some coordinates*, *some coordinates*) return integer
+ * validMove(*some coordinates*, *some coordinates*) return boolean
+ * tileAt(*some coordinates*) return Tile
+ * createBoard(*some array of tiles?*)
+ * createCheckerBoard(integer, integer)
+ * printBoard()
+ */
+
+public class Board {
+
+}
