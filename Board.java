ed4fdeb1839d2312db8f231864111a1413fa6030
Maxim Despinoy <maxdespinoy@gmail.com>
2020-09-26 19:39:05 +0100

    move added

--- Board.java
+++ Board.java
@@ -2,7 +2,7 @@
 /**
  * Class, for holding tile instances, should include the following functions:
  * 
- * move(*some coordinates*, *some coordinates*) return boolean
+ * move(*some coordinates*, *some coordinates*)
  * isPiece(*some coordinates*) return boolean
  * euclideanDistance(*some coordinates*, *some coordinates*) return integer
  * validMove(*some coordinates*, *some coordinates*) return boolean
@@ -18,6 +18,7 @@ public class Board {
     private Tile[][] boardTiles;
     private int boardWidth;
     private int boardHeight;
+    private boolean looping;
 
     /**
      * Defaults to 8x8 board (change method later?)
@@ -26,6 +27,7 @@ public class Board {
 
         this.boardHeight = 8;
         this.boardWidth = 8;
+        this.looping = false;
 
         this.boardTiles = new Tile[boardHeight][boardWidth];
 
@@ -45,8 +47,22 @@ public class Board {
      * @return - whether there is a piece at [x][y]
      */
     public boolean isPiece(int x, int y) {
+        return getTileAt(x-1, y-1).hasPiece();
+    }
 
-        return getTileAt(x-1, y-1).hasPiece;
+    /**
+     * move just moves a piece from one place to the next, it does not check if it is a valid move!
+     * 
+     * @param originX x position of the piece
+     * @param originY y position of the piece
+     * @param destX x position of the movement
+     * @param destY y position of the movement
+     */
+    public void move(int originX, int originY, int destX, int destY) {
+        if(onBoard(originX, originY) && onBoard(destX, destY)) {
+            this.boardTiles[destX][destY].piece = this.boardTiles[originX][originY].piece;
+            this.boardTiles[originX][originY].removePiece();
+        }
     }
 
     /**
@@ -60,5 +76,20 @@ public class Board {
         return this.boardTiles[x][y];
     }
 
-    
+    /**
+     * onBoard gets given a coordinate and returns whether the coordinate is on the board
+     * 
+     * @param x - the height of the given coordinate (which row is it in).
+     * @param y - how far along the row the piece is.
+     * @return - whether the coordinate is valid.
+     */
+    private boolean onBoard(int x, int y) {
+        if(!this.looping) {
+            return x <= boardHeight &&
+                   x > 0            &&
+                   y <= boardWidth  &&
+                   y > 0;
+        }
+        return true;
+    }
 }
